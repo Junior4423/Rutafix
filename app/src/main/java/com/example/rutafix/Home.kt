6671f8590eb3ca0c8371da0258032c5ba539ec60
Junior4423 <jc677189@gmail.com>
2026-02-25 20:00:43 -0500

    Primer commit - Taller terminado

---
+++ app/src/main/java/com/example/rutafix/Home.kt
@@ -0,0 +1,31 @@
+package com.example.rutafix
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val TextoRegistrate = findViewById<TextView>(R.id.textRegistrate)
+
+        TextoRegistrate.setOnClickListener {
+
+            val pasarLogin = Intent(this, Login::class.java)
+
+            startActivity(pasarLogin)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rutafix/Splash.kt
@@ -0,0 +1,37 @@
+package com.example.rutafix
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+
+
+            // se redirecciona a home
+            val intent = Intent(this, Home::class.java)
+
+            startActivity(intent)
+
+            // Cerrar el splash
+            finish()
+
+
+        },
+            3000)
+    }
+}
